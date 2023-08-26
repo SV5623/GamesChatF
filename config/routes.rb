b434a799f001dc0a2cd151e7682dfd0804bd10b5
SV5623 <ksv05623@gmail.com>
2023-08-26 15:06:38 +0200

    Added post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   get 'index', to: 'pages#index'
   get 'info', to: 'pages#info'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
